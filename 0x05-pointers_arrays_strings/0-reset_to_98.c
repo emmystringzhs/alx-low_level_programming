957872a75128d047c03f7ff38a57a32e874979ef
Emmanuel <milesbetti@gmail.com>
2022-10-24 11:38:17 -0700

    lets check

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
+#include "main.h"
 /**
  * reset_to_98-> fuction to change a pointer 
- *
+ * 
+ * @n: This is the entry
  * Return: return 0
  */
 
-void reset_to_98(int *n)
+void reset_to_958(int *n)
 {
 	*n = 98;
 	
-	return *n;
 }
