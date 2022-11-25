a0bd2241ab1f7507b56dfd7ec1ca3deba836e11d
Emmanuel <milesbetti@gmail.com>
2022-11-24 20:49:16 -0800

    hmm

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,5 @@
-#ifndef F50E5489_5AF7_408E_B7D5_811DCE5CED73
-#define F50E5489_5AF7_408E_B7D5_811DCE5CED73
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,4 +27,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* F50E5489_5AF7_408E_B7D5_811DCE5CED73 */
+#endif /* LISTS_H*/
